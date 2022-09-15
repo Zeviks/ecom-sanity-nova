047f558f9cacd72b579a26f20bc3c3b01b9fba17
Natanael Pacheco <natepacheco716@gmail.com>
2022-09-15 13:05:30 -0500

    Implemented Components

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+      </div>
+      <h3>MID TEXT</h3>
+      <img src="" alt="headphones" className="hero-banner-image" />
+    </div>
+  );
+};
+
+export default HeroBanner;
