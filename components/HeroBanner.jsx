af4593bbece814a5478c5be70955ad61a02223ae
Natanael Pacheco <natepacheco716@gmail.com>
2022-09-16 12:24:45 -0500

    Implented sanity cleint

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const HeroBanner = () => {
@@ -8,6 +9,15 @@ const HeroBanner = () => {
       </div>
       <h3>MID TEXT</h3>
       <img src="" alt="headphones" className="hero-banner-image" />
+      <div>
+        <Link href="/product/ID">
+          <button type="button">BUTTON TEXT</button>
+        </Link>
+        <div className="desc">
+          <h5>Description</h5>
+          <p>DESCRIPTION</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 
+import { client } from "../lib/client";
 import { Product, Footer, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <Footer />
     </>
   );
+};
+
+// Were fetching data that matches our query, using client.fetch and then passing the query to get the data
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  //Whatever gets returned from getServerSideProps is what is going to populate our dynamic fields.
+  return { bannerData, products };
 };
 
 export default Home;
