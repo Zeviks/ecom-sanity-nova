40b40267b415604d2e12cd22b504a8f371a5e813
Natanael Pacheco <natepacheco716@gmail.com>
2022-09-17 19:19:04 -0500

    Test

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,13 +2,15 @@ import React from "react";
 
 import { client, urlFor } from "../../lib/client";
 
-const ProductDetails = () => {
+const ProductDetails = ({ products, product }) => {
+  const { image, name, details, price } = product;
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div>
-            <img src="" />
+            <img src={urlFor(image && image[0])} />
           </div>
         </div>
       </div>
@@ -17,15 +19,16 @@ const ProductDetails = () => {
 };
 
 //getStaticProps is a function we use when we want to pre-render the page at build time ahead of user's request
+//! R-A-C
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-  const products = await client.fetch(query);
+  const productsQuery = '*[_type == "product"]';
 
-  const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
-  //Whatever gets returned from getServerSideProps is what is going to populate our dynamic fields.
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
   return {
-    props: { bannerData, products },
+    props: { products, product },
   };
 };
 
